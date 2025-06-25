1d0b84f53dec030eec6f61d9fe37087bc290f7bb
António Teca <antonioteca@hotmail.com>
2025-06-26 00:17:45 +0100

    fix: Update tab title from "Casa" to "Início" in TabLayout component

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -39,7 +39,7 @@ export default function TabLayout() {
             <Tabs.Screen
                 name="index"
                 options={{
-                    title: "Casa",
+                    title: "Início",
                     tabBarIcon: ({ color }) => (
                         <IconSymbol size={28} name="house.fill" color={color} />
                     ),
