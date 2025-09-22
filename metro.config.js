9d68a645f9839ad29e40388a9b4cbde865b12c9b
António Teca <antonioteca@hotmail.com>
2025-09-22 20:51:41 +0100

    Update dependencies in package.json
    - Bump expo version to ~54.0.9
    - Update expo-router to ~6.0.7
    - Add eventemitter3 dependency
    - Add metro-runtime dependency

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,15 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require("expo/metro-config");
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 // Garantir que o SDK do Firebase JS esteja empacotado corretamente
-config.resolver.sourceExts.push('cjs');
+config.resolver.sourceExts.push("cjs");
 config.resolver.unstable_enablePackageExports = false;
+
+config.resolver.extraNodeModules = {
+    ...config.resolver.extraNodeModules,
+    "./vendor/eventemitter3": require.resolve("eventemitter3"),
+};
 
 module.exports = config;
